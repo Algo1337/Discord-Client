973074b7e46774375dd0fceb1647b52461e736d7
Algo <142917637+Algo1337@users.noreply.github.com>
2026-06-26 08:28:32 -0500

    n

---
+++ Discord Client/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Discord_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(789, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resize Mode [ - ]";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(934, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(49, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discord Client";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(685, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Private Mode [ - ]";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 635);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Discord Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Discord Client/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Client
+{
+    public partial class Form1 : Form
+    {
+        Region _old;
+        public static int TogMove;
+        public static int MValX;
+        public static int MValY;
+        WebView2 w = new WebView2();
+
+        private const int HTLEFT = 10;
+        private const int HTRIGHT = 11;
+        private const int HTTOP = 12;
+        private const int HTTOPLEFT = 13;
+        private const int HTTOPRIGHT = 14;
+        private const int HTBOTTOM = 15;
+        private const int HTBOTTOMLEFT = 16;
+        private const int HTBOTTOMRIGHT = 17;
+
+        private const int resizeBorder = 8; // thickness in pixels
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/62fddf0fde45a8baedcc7ee5_847541504914fd33810e70a0ea73177e%20(2)-1.png");
+            this.Controls.Add(this.w);
+            this.w.Location = new Point(0, panel1.Height);
+            this.w.Size = new Size(this.Width, this.Height - panel1.Height);
+
+            setup_browser();
+            _old = this.Region;
+            this.round_corners(new Control[] { this });
+
+            panel1.MouseDown += panel1_MouseDown;
+            panel1.MouseMove += panel1_MouseMove;
+            panel1.MouseUp += panel1_MouseUp;
+
+            panel1.BringToFront();
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCHITTEST = 0x84;
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                Point pos = PointToClient(new Point(m.LParam.ToInt32()));
+
+                int grip = 8;
+
+                bool left = pos.X <= grip;
+                bool right = pos.X >= ClientSize.Width - grip;
+                bool top = pos.Y <= grip;
+                bool bottom = pos.Y >= ClientSize.Height - grip;
+
+                if (left && top)
+                    m.Result = (IntPtr)HTTOPLEFT;
+                else if (right && top)
+                    m.Result = (IntPtr)HTTOPRIGHT;
+                else if (left && bottom)
+                    m.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (right && bottom)
+                    m.Result = (IntPtr)HTBOTTOMRIGHT;
+                else if (left)
+                    m.Result = (IntPtr)HTLEFT;
+                else if (right)
+                    m.Result = (IntPtr)HTRIGHT;
+                else if (top)
+                    m.Result = (IntPtr)HTTOP;
+                else if (bottom)
+                    m.Result = (IntPtr)HTBOTTOM;
+                else
+                    m.Result = (IntPtr)1; // HTCLIENT
+
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+
+        public async void setup_browser()
+        {
+            await this.w.EnsureCoreWebView2Async();
+            this.w.CoreWebView2.Navigate("https://discord.com/channels/@me");
+        }
+
+        public void round_corners(Control[] Controls)
+        {
+            foreach (Control obj in Controls)
+            {
+                string type = obj.GetType().ToString().ToLower();
+                int d = 10;
+                if (obj.GetType().ToString() == "SWX.Form1")
+                {
+                    d = 30;
+                }
+
+                GraphicsPath gay = new GraphicsPath();
+                gay.AddArc(0, 0, d, d, 180, 90);
+                gay.AddArc(obj.Width - d, 0, d, d, 270, 90);
+                gay.AddArc(obj.Width - d, obj.Height - d, d, d, 0, 90);
+                gay.AddArc(0, obj.Height - d, d, d, 90, 90);
+                gay.CloseAllFigures();
+                obj.Region = new Region(gay);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e) { TogMove = 1; MValX = e.X; MValY = e.Y; }
+        private void panel1_MouseMove(object sender, MouseEventArgs e) { if (TogMove == 1) { base.SetDesktopLocation(Control.MousePosition.X - MValX, Control.MousePosition.Y - MValY); } }
+        private void panel1_MouseUp(object sender, MouseEventArgs e) { TogMove = 0; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Region = this._old;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (label3.Text.Contains("[ - ]"))
+            {
+                label3.Text = label3.Text.Replace("[ - ]", "[ + ]");
+                this.Region = _old;
+                this.w.Location = new Point(5, panel1.Height);
+                this.w.Size = new Size(this.Width - 10, this.Height - panel1.Height - 5);
+            } else
+            {
+                label3.Text = label3.Text.Replace("[ + ]", "[ - ]");
+                this.round_corners(new Control[] { this });
+                this.w.Location = new Point(0, panel1.Height);
+                this.w.Size = new Size(this.Width, this.Height - panel1.Height);
+            }
+        }
+
+        private async void label4_Click(object sender, EventArgs e)
+        {
+            WebView2 p = new WebView2();
+            await p.EnsureCoreWebView2Async();
+
+            var env = await CoreWebView2Environment.CreateAsync(
+                userDataFolder: null
+            );
+            p.CoreWebView2.Navigate("https://discord.com/channels/@me");
+            this.Controls.Add(p);
+        }
+        private async void label4_Click_1(object sender, EventArgs e)
+        {
+            this.w.Dispose();
+            this.w = new WebView2();
+            this.Controls.Add(this.w);
+            this.w.Location = new Point(0, panel1.Height);
+            this.w.Size = new Size(this.Width, this.Height - panel1.Height);
+            var env = await CoreWebView2Environment.CreateAsync();
+            var options = env.CreateCoreWebView2ControllerOptions();
+            options.IsInPrivateModeEnabled = true;
+            await this.w.EnsureCoreWebView2Async(env, options);
+            this.w.CoreWebView2.Navigate("https://discord.com/channels/@me");
+        }
+    }
+}
